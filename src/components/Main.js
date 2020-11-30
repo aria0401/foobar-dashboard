487e33a7a20d73a748c42370862baefa0321a36b
suppmen <suppmen@gmail.com>
2020-11-30 11:04:06 +0100

    fixing the merge on Laufey ready-togo component

--- src/components/Main.js
+++ src/components/Main.js
@@ -6,46 +6,27 @@ import ReadyList from "./ReadyList";
 import Crew from "./Crew";
 import Taps from "./Taps";
 
-<<<<<<< HEAD
-function Main() {
-  const [data, setData] = useState({});
-  const [beers, setBeers] = useState({});
-  console.log(data);
-  console.log(beers);
-  useEffect(() => {
-    getData(setData);
-    getBeers(setBeers);
 
-    setInterval(() => {
-      getData(setData);
-    }, 10000);
-    getData(setData);
-  }, []);
-=======
+
+
 function Main(props) {
   
   const data = props.data;
->>>>>>> 0af2e047e6aaa366a6bac80a8a8ba66688fcfd80
+
 
   return (
     <div className="Main">
-<<<<<<< HEAD
-      {data.bar && <Clock data={data} />}
-      {data.serving && <Serving data={data} />}
-      {data.serving && <ReadyList data={data} />}
-      {data.bartenders && <Crew data={data} />}
-      <Taps />
-      {data.queue && <Queue data={data} />}
-=======
+
      
       <Clock data={data} />
       <Serving data={data} />
-      <ReadyList />
+      <ReadyList data={data} />
       <Crew data={data}/>
       <Taps data={data}/>
       <Queue data={data} />
+      <ReadyList data={data} />
      
->>>>>>> 0af2e047e6aaa366a6bac80a8a8ba66688fcfd80
+
     </div>
   );
 }
