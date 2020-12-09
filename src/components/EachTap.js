8dad3a1a774d3c91cbb4d5e8d48e87d0474875e3
Ariadna <aria0401@kea.stud.dk>
2020-12-09 23:13:33 +0100

    responsive taps

--- src/components/EachTap.js
+++ src/components/EachTap.js
@@ -1,38 +1,15 @@
 import React from "react";
 
 export default function EachTap(props){
-  const level = props.item.level/13;
-  const capacity = props.item.capacity/13;
+  const capacity = props.item.capacity/200;
+  const level = props.item.level/200;
 
 
     return(
-      <div> 
-
-    <div className="level" style={{
-      background:"#E39C96",
-      height: level,
-      marginTop:"5px",
-      marginLeft:"5px",
-      marginRight: "5px",
-      borderRadius: "5px",
-    }}>
-      <p>
-      {props.item.level}
-      </p>
-      </div>
-
-      {/* <div className="capacity" style={{
-      background:"#F6C83F",
-      height: capacity,
-      marginTop:"5px",
-      marginLeft:"5px",
-      marginRight: "5px",
-      borderRadius: "5px",
-    }}>
-      <p>
-      {props.item.level}
-      </p>
-      </div> */}
+      <div className="each-tap">
+        <div className="capacity" style={{height: capacity+ "vw"}}></div>
+        <div className="level" style={{height: level + "vw"}}></div>
+        <p>{props.item.level}</p>
       </div>
     )
   }
--- src/components/Taps.js
+++ src/components/Taps.js
@@ -10,68 +10,14 @@ function Taps(props) {
       <h1 className="header-taps">Taps</h1>
 
       <div className="tap-bars">
-
-     {tapsArray && tapsArray.map((item, index) => {
-         
-          return (
-            <EachTap key={index} item={item} data={props.data} />
-      
-          );
-        })}
-         </div>
+        {tapsArray && tapsArray.map((item, index) => {
+           return (
+          <EachTap key={index} item={item} data={props.data} />
+          ) 
+          })}
+      </div>
     </div>
   ) 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function Taps(props) {
-//   const tapsArray = props.data.taps;
-
-//   return (
-//     <div className="Taps panel ">
-//       <h1>Taps</h1>
-
-//       <div> 
-
-//       <svg className="svgTaps" viewBox="0 0 100 100" > 
-
-//       {tapsArray &&
-//         tapsArray.map((item, index) => {
-//           const height = item.capacity/50;
-//           return (
-//             <rect  key={index} fill="#F6C83F" x={item.id*12} y="0" width="7" rx="2" ry="2" height={height} />
-//           );
-//         })}
-
-//         {tapsArray &&
-//         tapsArray.map((item, index) => {
-//           const height = item.level/50;
-//           return (
-//             <rect  key={index} fill="#E39C96" x={item.id*12} y="0" width="7" rx="2" ry="2" height={height} />
-     
-//           );
-//         })}
-
-//       </svg>
-//       </div>
-//     </div>
-//   );
-  
-// }
-
-
-
 
 export default Taps;
