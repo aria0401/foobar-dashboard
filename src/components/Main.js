e28b8c3e7cf3fd358348ca4b44f65670b698d0c5
nitzanmoise <nitzanmois@gmail.com>
2020-12-10 16:33:05 +0100

    conflicts

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,10 +2,7 @@ import React from "react";
 import Queue from "./Queue";
 import Clock from "./Clock";
 import Serving from "./Serving";
-<<<<<<< HEAD
-=======
 import MostSoldBeer from "./MostSoldBeer";
->>>>>>> 195a0d184a507c689693cf58b86963bff0649398
 import Crew from "./Crew";
 import Taps from "./Taps";
 
@@ -16,19 +13,13 @@ function Main(props) {
     <div className="Main">
       <Clock data={data} />
       <Serving data={data} />
-<<<<<<< HEAD
       <Crew data={data} />
       <Taps data={data} />
       <Queue data={data} />
-=======
       <MostSoldBeer data={data} />
-      <Crew data={data}/>
-      <Taps data={data}/>
+      <Crew data={data} />
+      <Taps data={data} />
       <Queue data={data} />
-     
-     
-
->>>>>>> 195a0d184a507c689693cf58b86963bff0649398
     </div>
   );
 }
