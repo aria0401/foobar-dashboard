0af33f8c4ef12f7b20966f845a7b2b00d204b83c
suppmen <suppmen@gmail.com>
2020-12-10 11:37:19 +0100

    image adjustments

--- src/components/MostSoldBeer.js
+++ src/components/MostSoldBeer.js
@@ -26,10 +26,9 @@ console.log(tapsName)
 
   return (
     <div className="MostSoldBeer panel">
-      <h1 id="highlight-title">Most sold at the moment</h1>
+      <h1 id="highlight-title">Most Wanted Tap</h1>
    
-       <div className="readyNr">
-       
+      
        
         {beerImages.map((beerImage, index) => {
           if (tapsName[0] === beerImage.name) {
@@ -46,7 +45,8 @@ console.log(tapsName)
           }
           return<div key={index}></div>
 })}
-        <p> <span>Number One:</span> {firstOne.name}</p> 
+ <div className="readyNr">
+        <p>{firstOne.name}</p> 
        
         </div>
     </div>
