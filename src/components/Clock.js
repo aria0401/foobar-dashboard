11852a4ca91f9943c4f1877816d6c90671562595
suppmen <suppmen@gmail.com>
2020-12-13 11:24:02 +0100

    issue with clock remaining time in sec

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -1,13 +1,12 @@
-import  React, { useState, useEffect} from 'react'
+import  React, { useState} from 'react'
 import AnalogClock from 'analog-clock-react';
 
 function Clock(props) {
 
 const [time, setTime] = useState("");
 
-let date = new Date();
 let options = {
-  
+  "useCustomTime": false,
   width: "150px",
   border: true,
   borderColor: "#F7B71E",
@@ -19,17 +18,13 @@ let options = {
     minute: "#F7B71E",
     hour: "#F7B71E"
   },
-  seconds: date.getSeconds(),
-  minutes: date.getMinutes(),
-  hours: date.getHours()
 };
 
-// this.interval = setInterval(this.updateClock, 1000);
 
 
 // inspired from https://ww.devcamp.com/trails/31/campsites/261/guides/calculating-time-react
 // remaining time to close the foobar
-function getTime() {
+function closeTime() {
   const date = new Date();
   const NewDate = date.getDate();
   const Month = date.getMonth() + 1;
@@ -40,29 +35,29 @@ function getTime() {
      
   const differnce = closingTime - date;
 
-      // let days = Math.floor(differnce / (1000 * 60 * 60 * 24));
+  
       let hours = Math.floor((differnce % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
       let minutes = Math.floor((differnce % (1000 * 60 * 60)) / (1000 * 60));
-      // let seconds = Math.floor((differnce % (1000 * 60)) / 1000);
+      let seconds = Math.floor((differnce % (1000 * 60)) / 1000);
 
       setTime(
-        `${hours < 10 ? `0${hours}` : hours}h & ${minutes < 10 ? `0${minutes}` : minutes }m`
-                );
+        `${hours < 10 ? `0${hours}` : hours}:${minutes < 10 ? `0${minutes}` : minutes }:${seconds < 10 ? `0${seconds}` : seconds }`);
+
                 if (differnce  < 0) {
                  setTime("we are closed")
               }
 }
 
-useEffect(() => {
-  const interval = setInterval(() => {
-    getTime();
-  }, 1000);
-  return () => clearInterval(interval);
-}, []);
+// useEffect(() => {
+//   const interval = setInterval(() => {
+//     getTime();
+//   }, 1000);
+//   return () => clearInterval(interval);
+// }, []);
     
-// setInterval(
-//   getTime,
-//  1000);
+setInterval(
+  closeTime,
+ 1000);
 
 
 return(
@@ -71,7 +66,7 @@ return(
       <div className="clock">   
         <AnalogClock {...options} />
       </div>
-     <h1 className="title" >Closing In: {time}</h1>
+     <h1 className="title" >Closing In {time}</h1>
      
     
     </div>
