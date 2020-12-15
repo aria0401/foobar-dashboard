85abb07bb5dc51c659c5d939cf776c16b526629d
Laufey <laufey@gmail.com>
2020-12-15 22:54:21 +0000

    responsive

--- src/components/EachTap.js
+++ src/components/EachTap.js
@@ -1,16 +1,14 @@
-
 import React from "react";
 
-export default function EachTap(props){
-  const capacity = props.item.capacity/145;
-  const level = props.item.level/150;
+export default function EachTap(props) {
+  const capacity = props.item.capacity / 125;
+  const level = props.item.level / 130;
 
-
-    return(
-      <div className="each-tap">
-        <div className="capacity" style={{height: capacity+ "vw"}}></div>
-        <div className="level" style={{height: level + "vw"}}></div>
-        <p>{props.item.beer}</p>
-      </div>
-    )
-  }
+  return (
+    <div className="each-tap">
+      <div className="capacity" style={{ height: capacity + "vh" }}></div>
+      <div className="level" style={{ height: level + "vh" }}></div>
+      <p>{props.item.beer}</p>
+    </div>
+  );
+}
